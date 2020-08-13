aa34e43bf58ca91b05e85dfd9a16c42ba2bf50c9
michelle <michelle.johansson85@gmail.com>
2020-08-13 11:16:29 +0200

    init commit

---
+++ jpa-exercises/src/main/java/se/lexicon/michelle/jpaexercises/JpaExercisesApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.michelle.jpaexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaExercisesApplication.class, args);
+	}
+
+}
---
+++ jpa-exercises/src/test/java/se/lexicon/michelle/jpaexercises/JpaExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.michelle.jpaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
